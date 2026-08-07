2e05cae5bc044d788772e88ef4ced93c22aa70e3
gosti21 <sergiozam459@gmail.com>
2026-08-07 13:16:11 -0500

    d3

--- anttec-front-master/src/services/apiConfig.ts
+++ anttec-front-master/src/services/apiConfig.ts
@@ -1,5 +1,5 @@
-const RENDER_PRIMARY_API_URL = 'https://anttec-back-yjry.onrender.com/api/v1'
-const RENDER_SECONDARY_API_URL = 'https://iccontinental.onrender.com/api/v1'
+const RENDER_PRIMARY_API_URL = 'https://iccontinental-1-m0s1.onrender.com/api/v1'
+const RENDER_SECONDARY_API_URL = 'https://anttec-back-yjry.onrender.com/api/v1'
 
 const rawEnvApiUrl = (import.meta.env.VITE_API_URL as string | undefined)?.trim()
 const isLocalEnvApiUrl =
