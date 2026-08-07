75835a6c505add9295e7f34b4bae0931440b21fb
gosti21 <sergiozam459@gmail.com>
2026-08-07 15:06:33 -0500

    r4

--- anttec-back-master/config/cors.php
+++ anttec-back-master/config/cors.php
@@ -19,7 +19,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => array_filter([
+        env('APP_URL_FRONT', 'http://localhost:5173'),
+        'http://localhost:5173',
+        'https://iccontinental-1-m0s1.onrender.com',
+        'https://iccontinental-lico.onrender.com',
+    ]),
 
     'allowed_origins_patterns' => [],
 
