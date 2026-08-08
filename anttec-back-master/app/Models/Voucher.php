123b624040849c9b79f9efa82e26d8f5517f15ab
gosti21 <sergiozam459@gmail.com>
2026-08-07 23:13:59 -0500

    redeploy

--- anttec-back-master/app/Models/Voucher.php
+++ anttec-back-master/app/Models/Voucher.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class Voucher extends Model
 {
@@ -13,6 +15,19 @@ class Voucher extends Model
         'path',
         'order_id',
     ];
+
+    protected function path(): Attribute
+    {
+        return Attribute::make(
+            get: function (?string $value): ?string {
+                if (! $value || Str::startsWith($value, ['http://', 'https://'])) {
+                    return $value;
+                }
+
+                return rtrim((string) config('app.url'), '/').'/'.ltrim($value, '/');
+            },
+        );
+    }
 
     public function order(): BelongsTo
     {
