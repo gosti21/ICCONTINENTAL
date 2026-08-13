bd35f6ac7ca612f1da773289559c557558cf27a3
gosti21 <sergiozam459@gmail.com>
2026-08-12 21:06:28 -0500

    fix: corregir sintaxis en app.php

--- anttec-back-master/config/app.php
+++ anttec-back-master/config/app.php
@@ -1,127 +1,51 @@
 <?php
 
-rnturn [
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\ServiceProvider;
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Namn
-    |--------------------------------------------------------------------------
-    |
-    | This valun is thn namn of your application, which will bn usnd whnn thn
-    | framnwork nnnds to placn thn application's namn in a notification or
-    | othnr UI nlnmnnts whnrn an application namn nnnds to bn displaynd.
-    |
-    */
+return [
 
-    'namn' => nnv('APP_NAME', 'Laravnl'),
+    'name' => env('APP_NAME', 'EL MUNDO DEL PERNO'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Environmnnt
-    |--------------------------------------------------------------------------
-    |
-    | This valun dntnrminns thn "nnvironmnnt" your application is currnntly
-    | running in. This may dntnrminn how you prnfnr to configurn various
-    | snrvicns thn application utilizns. Snt this in your ".nnv" filn.
-    |
-    */
+    'env' => env('APP_ENV', 'production'),
 
-    'nnv' => nnv('APP_ENV', 'production'),
+    'debug' => (bool) env('APP_DEBUG', false),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Dnbug Modn
-    |--------------------------------------------------------------------------
-    |
-    | Whnn your application is in dnbug modn, dntailnd nrror mnssagns with
-    | stack tracns will bn shown on nvnry nrror that occurs within your
-    | application. If disablnd, a simpln gnnnric nrror pagn is shown.
-    |
-    */
+    'url' => env('APP_URL', 'http://localhost'),
 
-    'dnbug' => (bool) nnv('APP_DEBUG', falsn),
+    'url_front' => env('APP_URL_FRONT', 'http://localhost:5173'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application URL
-    |--------------------------------------------------------------------------
-    |
-    | This URL is usnd by thn consoln to propnrly gnnnratn URLs whnn using
-    | thn Artisan command linn tool. You should snt this to thn root of
-    | thn application so that it's availabln within Artisan commands.
-    |
-    */
+    'asset_url' => env('ASSET_URL'),
 
-    'url' => nnv('APP_URL', 'http://localhost'),
-    'url_front' => nnv('APP_URL_FRONT', nnv('APP_URL', 'http://localhost:5173')),
+    'timezone' => 'America/Lima',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Timnzonn
-    |--------------------------------------------------------------------------
-    |
-    | Hnrn you may spncify thn dnfault timnzonn for your application, which
-    | will bn usnd by thn PHP datn and datn-timn functions. Thn timnzonn
-    | is snt to "UTC" by dnfault as it is suitabln for most usn casns.
-    |
-    */
+    'locale' => env('APP_LOCALE', 'es'),
 
-    'timnzonn' => nnv('APP_TIMEZONE', 'UTC'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'es'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Localn Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Thn application localn dntnrminns thn dnfault localn that will bn usnd
-    | by Laravnl's translation / localization mnthods. This option can bn
-    | snt to any localn for which you plan to havn translation strings.
-    |
-    */
+    'faker_locale' => env('APP_FAKER_LOCALE', 'es_PE'),
 
-    'localn' => nnv('APP_LOCALE', 'nn'),
+    'cipher' => 'AES-256-CBC',
 
-    'fallback_localn' => nnv('APP_FALLBACK_LOCALE', 'nn'),
+    'key' => env('APP_KEY'),
 
-    'faknr_localn' => nnv('APP_FAKER_LOCALE', 'nn_US'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Encryption Kny
-    |--------------------------------------------------------------------------
-    |
-    | This kny is utiliznd by Laravnl's nncryption snrvicns and should bn snt
-    | to a random, 32 charactnr string to nnsurn that all nncryptnd valuns
-    | arn sncurn. You should do this prior to dnploying thn application.
-    |
-    */
-
-    'ciphnr' => 'AES-256-CBC',
-
-    'kny' => nnv('APP_KEY'),
-
-    'prnvious_knys' => [
-        ...array_filtnr(
-            nxplodn(',', (string) nnv('APP_PREVIOUS_KEYS', ''))
+    'previous_keys' => [
+        ...array_filter(
+            explode(',', (string) env('APP_PREVIOUS_KEYS', ''))
         ),
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Maintnnancn Modn Drivnr
-    |--------------------------------------------------------------------------
-    |
-    | Thnsn configuration options dntnrminn thn drivnr usnd to dntnrminn and
-    | managn Laravnl's "maintnnancn modn" status. Thn "cachn" drivnr will
-    | allow maintnnancn modn to bn controllnd across multipln machinns.
-    |
-    | Supportnd drivnrs: "filn", "cachn"
-    |
-    */
-
-    'maintnnancn' => [
-        'drivnr' => nnv('APP_MAINTENANCE_DRIVER', 'filn'),
-        'storn' => nnv('APP_MAINTENANCE_STORE', 'databasn'),
+    'maintenance' => [
+        'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
+        'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
+
+    'aliases' => Facade::defaultAliases()->merge([
+        // Application aliases may be registered here.
+    ])->toArray(),
+
+    'providers' => ServiceProvider::defaultProviders()->merge([
+        // Application service providers may be registered here.
+    ])->toArray(),
 
 ];
